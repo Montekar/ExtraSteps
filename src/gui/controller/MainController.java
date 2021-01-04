3623b91558fecd773d3aaa0c493900c6ba46517f
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-01-04 21:41:13 +0100

    Add Movie class was added Add Movie method added until the Dao layer Alert class and Movie Manager created

--- src/gui/controller/MainController.java
+++ src/gui/controller/MainController.java
@@ -1,12 +1,22 @@
 package gui.controller;
 
+import gui.model.MovieModel;
 import javafx.event.ActionEvent;
 
 import java.awt.*;
 import java.net.URL;
+import java.util.Arrays;
 
 public class MainController {
+
+    MovieModel movieModel = new MovieModel();
+
     public void addMovie(ActionEvent actionEvent) {
+        String[] result = AddMovie.display("Add Movie", "Fill the fields to add new Movie");
+
+        boolean canAdd = Arrays.stream(result).anyMatch(a -> a != null && !a.isEmpty());
+
+        if (canAdd) movieModel.addMovie(result[0], result[1], result[2], result[3], result[4]);
     }
 
     public void editMovie(ActionEvent actionEvent) {
